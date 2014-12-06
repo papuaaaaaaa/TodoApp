4d41a834b46526420bb33e98f4759a535d8682ec
papua <nakao_shinichirou@aratana.jp>
2014-12-06 13:59:27 +0900

    use ng-controller ng-submit ng-repeat

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
 /**
  * Created by papua on 2014/12/06.
  */
-angular.module('App', []);
+angular.module('App', [])
+    .controller('MainController', ['$scope', function($scope) {
+
+        $scope.todos = [];
+
+        $scope.addTodo = function() {
+            console.log('call addTodo');
+            $scope.todos.push({
+                title: Math.random(),
+                done: false
+            });
+        };
+
+    }]);
+
