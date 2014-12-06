42c90f0e295a2882f798782d994b25d04c2d26b1
papua <nakao_shinichirou@aratana.jp>
2014-12-06 14:23:49 +0900

    use ng-model ng-class

--- js/app.js
+++ js/app.js
@@ -5,13 +5,16 @@ angular.module('App', [])
     .controller('MainController', ['$scope', function($scope) {
 
         $scope.todos = [];
+        $scope.newTitle = '';
 
         $scope.addTodo = function() {
             console.log('call addTodo');
             $scope.todos.push({
-                title: Math.random(),
+                title: $scope.newTitle,
                 done: false
             });
+            //双方向
+            $scope.newTitle = '';
         };
 
     }]);
